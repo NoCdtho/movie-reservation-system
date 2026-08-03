9bafb120316a41292888ad009a9045c9b7b8f5b5
Rohit Gupta <shecded@gmail.com>
2026-08-03 22:12:30 +0530

    Update seat java model

--- src/main/java/com/nocde/movie_reservation_system/model/seat.java
+++ src/main/java/com/nocde/movie_reservation_system/model/seat.java
@@ -1,5 +1,22 @@
 package com.nocde.movie_reservation_system.model;
 
+import jakarta.persistence.*;
+
+@Entity
+
+@Table(name = "seat", uniqueConstraints = {
+    @UniqueConstraint(name = "seat_row", columnNames = {"seat_row", "seat_number"})
+})
+
 public class seat {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seat_id", nullable = false)
+    private Integer seatId;
+
+    @Column(name = "seat_row", nullable = false, length = 5)
+    private String seatRow;
+
+    @Column(name = "seat_number", nullable = false)
+    private Integer seatNumber;
 }
