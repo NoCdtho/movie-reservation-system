ddf4b04c6ed38e88ee8c15fe6108a6d620a5912f
Rohit Gupta <shecded@gmail.com>
2026-08-14 06:24:05 +0530

    commented unneccessary files

--- src/main/java/com/nocde/movie_reservation_system/controller/movieController.java
+++ src/main/java/com/nocde/movie_reservation_system/controller/movieController.java
@@ -1,30 +1,30 @@
-package com.nocde.movie_reservation_system.controller;
+// package com.nocde.movie_reservation_system.controller;
 
-import com.nocde.movie_reservation_system.model.movie;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+// import com.nocde.movie_reservation_system.model.movie;
+// import org.springframework.web.bind.annotation.*;
+// import java.util.List;
 
-//This is a spring annotation used for building restfull webservice this returns data directly (like JSON or XML)
-@RestController
-//This is a spring annotation used to MAP incoming HTTP requests to a specific controller class.
-@RequestMapping("/movies") 
-public class movieController {
+// //This is a spring annotation used for building restfull webservice this returns data directly (like JSON or XML)
+// @RestController
+// //This is a spring annotation used to MAP incoming HTTP requests to a specific controller class.
+// @RequestMapping("/movies") 
+// public class movieController {
 
-    /*
-    This spring framework annotation maps the HTTP GET requests to specific methods in a controller 
-    below no parenthesis is passed therefore it inherits the RequestMapping path that is /movies
-    */
-    // @GetMapping 
-    // // public List<movie> getAllMovies(){
-    // //     // return List.of(
-    // //     //     new movie(1L, "Inception", "Goted", 2),
-    // //     //     new movie(2L, "Intersteller", "Second Goated", 3),
-    // //     //     new movie(3L, "The Dark Knight", "myGod", 4)
-    // //     // );
-    // // }
+//     /*
+//     This spring framework annotation maps the HTTP GET requests to specific methods in a controller 
+//     below no parenthesis is passed therefore it inherits the RequestMapping path that is /movies
+//     */
+//     // @GetMapping 
+//     // // public List<movie> getAllMovies(){
+//     // //     // return List.of(
+//     // //     //     new movie(1L, "Inception", "Goted", 2),
+//     // //     //     new movie(2L, "Intersteller", "Second Goated", 3),
+//     // //     //     new movie(3L, "The Dark Knight", "myGod", 4)
+//     // //     // );
+//     // // }
 
-    // @GetMapping("/{id}")
-    // public movie getMovieById(@PathVariable Long id) {
-    //     return new movie(id, "Inception", "Sci-Fi", 148);
-    // }
-}
+//     // @GetMapping("/{id}")
+//     // public movie getMovieById(@PathVariable Long id) {
+//     //     return new movie(id, "Inception", "Sci-Fi", 148);
+//     // }
+// }
