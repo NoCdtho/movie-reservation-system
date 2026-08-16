607aaccc79c1c6eabfe4f40ba60a9d71896035f1
Rohit Gupta <shecded@gmail.com>
2026-08-16 11:05:47 +0530

    Rought implementation

---
+++ src/main/java/com/nocde/movie_reservation_system/data_access_layer/movie_repository_interface/movie_DAO_implementation.java
@@ -0,0 +1,29 @@
+package com.nocde.movie_reservation_system.data_access_layer.movie_repository_interface;
+
+import com.nocde.movie_reservation_system.model.movie;
+import java.util.*;
+
+public class movie_DAO_implementation implements movie_DAO{
+
+    public void addMovie(movie movie){
+        
+    }
+
+    public movie getMovieById(int id){
+        movie m = null;
+        return m;
+    }
+
+    public List<movie> getAllMovie(){
+        List<movie> m = new ArrayList<>();
+        return m;
+    }
+
+    public void updateMovie(movie movie){
+
+    }
+
+    public void deleteMovie(int id){
+
+    }
+}
