dab31258fb4c03a85a9f4482fe04871b7a678e6b
Rohit Gupta <shecded@gmail.com>
2026-08-16 11:57:20 +0530

    Update addMovie Execute the SQL query

--- src/main/java/com/nocde/movie_reservation_system/data_access_layer/movie_repository_interface/movie_DAO_implementation.java
+++ src/main/java/com/nocde/movie_reservation_system/data_access_layer/movie_repository_interface/movie_DAO_implementation.java
@@ -1,12 +1,35 @@
 package com.nocde.movie_reservation_system.data_access_layer.movie_repository_interface;
 
+import com.nocde.movie_reservation_system.data_access_layer.databaseManager;
 import com.nocde.movie_reservation_system.model.movie;
-import java.util.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+import java.time.*;
 
 public class movie_DAO_implementation implements movie_DAO{
 
     public void addMovie(movie movie){
-        
+        String SQL = "INSERT INTO movie (title, description, duration_mins, release_date) values (?, ?, ?, ?)"; 
+        try(
+            Connection con = databaseManager.getConnection();
+            PreparedStatement prep = con.prepareStatement(SQL);
+        ){
+            prep.setString(1, movie.getTitle());
+            prep.setString(2, movie.getDescription());
+            prep.setInt(3, movie.getDuration());
+
+            // convert the releaseDate of type LocalDate (java.time.data) into Date (java.sql.time)
+            LocalDate localdate = movie.getReleaseDate();
+            Date date = Date.valueOf(localdate);
+            prep.setDate(4, date);
+
+            prep.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public movie getMovieById(int id){
