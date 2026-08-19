5fe24a63c2ee912d8612d59f1ee2c4fdd8eec7db
Rohit Gupta <shecded@gmail.com>
2026-08-20 02:42:40 +0530

    Jpa for seat repository

---
+++ src/main/java/com/nocde/movie_reservation_system/repository/seatRepository.java
@@ -0,0 +1,17 @@
+package com.nocde.movie_reservation_system.repository;
+
+import com.nocde.movie_reservation_system.model.seat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface seatRepository extends JpaRepository<seat, Integer>{
+    //find seat in a spefic row
+    List<seat> findBySeatRow(String seatRow);
+
+    //  find specific row using seat number and seat row
+    Optional<seat> findBySeatRowAndSeatNumber(String seatRow, Integer seatNumber);
+}
