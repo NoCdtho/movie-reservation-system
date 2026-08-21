e6532a50dee5b4746b16f20b983879044111e31a
Rohit Gupta <shecded@gmail.com>
2026-08-21 16:58:39 +0530

    Service for booking JPA

--- src/main/java/com/nocde/movie_reservation_system/Service/BookingService.java
+++ src/main/java/com/nocde/movie_reservation_system/Service/BookingService.java
@@ -3,6 +3,9 @@ import com.nocde.movie_reservation_system.repository.bookingRepository;
 import org.springframework.stereotype.Service;
 import com.nocde.movie_reservation_system.model.booking;
 import com.nocde.movie_reservation_system.model.User;
+import com.nocde.movie_reservation_system.model.showTime;
+import com.nocde.movie_reservation_system.model.booking.bookingStatus;
+import com.nocde.movie_reservation_system.model.seat;
 import java.util.*;
 
 @Service
@@ -13,16 +16,30 @@ public class BookingService {
         this.booking_repository = booking_repository;
     }
 
-    // Error: Not handled in this function call 
-    // Retrive a particular booking
-    public booking getBookingById(int bookingId) {
-        return booking_repository.findByBookingId(bookingId);
-    }
-
-    // Retrive all booking made by a user
+    // Retrive all booking made by a user id
     public List<booking> getAllBookings(User userId){
         return booking_repository.findByUserId(userId);
     }
 
+    // Retrive all the booking for a specific user using showTime id 
+    public List<booking> getByShowTimeId(showTime showTimeId){
+        return booking_repository.findByShowTimeId(showTimeId);
+    }
+
+    // Retrive all booking based on their booking status
+    public List<booking> getByStatus(bookingStatus status){
+        return booking_repository.findByStatus(status);
+    }
+
+    // Retrive the seat is booked or not for a particular showtime
+    public boolean IsSeatBooked(showTime showTimeId, seat seatId){
+        return booking_repository.existsByShowTimeIdAndSeatId(showTimeId, seatId);
+    }
+
+    // Error: Not handled in this function call 
+    // Retrive a particular booking
+    public booking getBookingById(int bookingId) {
+        return booking_repository.findByBookingId(bookingId);
+    } 
     
 }
